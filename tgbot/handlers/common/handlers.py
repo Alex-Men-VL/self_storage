640ce4569953095fe6bb1077100fc5212a9270b8
Aleksandr <menshikov.a.v@icloud.com>
2021-11-18 15:48:10 +0300

    FIX

--- tgbot/handlers/common/handlers.py
+++ tgbot/handlers/common/handlers.py
@@ -8,7 +8,7 @@ from .keyboard_utils import make_keyboard_for_start_command
 def command_start(update: Update, context) -> None:
     user_info = update.message.from_user.to_dict()
     user, created = StorageUser.objects.get_or_create(
-        user_id=user_info['id'],
+        telegram_id=user_info['id'],
         username=user_info['username'],
         first_name=user_info['first_name'],
         last_name=user_info['last_name'],
