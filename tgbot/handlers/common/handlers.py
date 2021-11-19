63d58972a48d241800ee7adeb3799a089d4f3000
Aleksandr <menshikov.a.v@icloud.com>
2021-11-19 15:44:13 +0300

    Add username in bd

--- tgbot/handlers/common/handlers.py
+++ tgbot/handlers/common/handlers.py
@@ -8,7 +8,8 @@ from .keyboard_utils import make_keyboard_for_start_command
 def command_start(update: Update, context) -> None:
     user_info = update.message.from_user.to_dict()
     user, created = StorageUser.objects.get_or_create(
-        telegram_id=user_info['id']
+        telegram_id=user_info['id'],
+        username=user_info['username']
     )
 
     if created:
